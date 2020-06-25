950b185b4fd831ca3c5a3772d4c813b2574bab35
Tassilo Balbo <contact@filipsikora.com>
2020-06-25 20:53:24 +0200

    index.js is now main.js. wow! great!

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	mode: 'none',
-	entry: './src/index.js',
+	entry: './src/main.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'public'),

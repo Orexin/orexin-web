33ab261ec5f6879cf2a864d94eeaef4b8f9f0277
Tassilo Balbo <contact@filipsikora.com>
2020-06-25 22:17:08 +0200

    organizace js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos';
 import 'jquery';
+import './viewstl/stl_viewer.min';
+import './contact-form';
+import './autocomplete';
+import './parallax';
+import './type-writer';
 
+// stl viewer
+var stl_viewer=new StlViewer
+(document.getElementById("stl_cont"),
+    {
+        canvas_width:"100%",
+        canvas_height:"75%",
+        zoom:1,
+        mouse_zoom:false,
+        auto_rotate:true,
+        models: 
+        [ 
+            {filename:"assets/orexin.stl",scale:1}
+        ] 
+	});
 
 firebase.initializeApp(firebaseConfig);

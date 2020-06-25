04f0431a31071193d1da2fb25de3022266c8b3ac
Tassilo Balbo <contact@filipsikora.com>
2020-06-25 20:51:41 +0200

    lul mistake

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as firebase from 'firebase/app';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'aos';
+import 'jquery';
+
+
+firebase.initializeApp(firebaseConfig);

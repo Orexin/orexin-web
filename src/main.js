9f11001e2a3e5c3dbb9472f503afa5dcaca482a1
Tassilo Balbo <contact@filipsikora.com>
2021-01-20 00:25:45 +0100

    skew

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // CSS
 import './style.css';
 import 'bulma/css/bulma.min.css';
+import 'aos/dist/aos.css';
+import * as skew from './js/onscroll-skew';
+
+//JS
 import AOS from 'aos';
 
-import './js/onscroll-skew';
-
 AOS.init({once: true});
+skew.onscrollSkew();

d11406b1f2670b4d72b1fc7cb3479189921886b7
turniXXD <legokubik@seznam.cz>
2021-01-20 16:36:22 +0100

    skew-porad-shit

--- src/js/onscroll-skew.js
+++ src/js/onscroll-skew.js
@@ -1,18 +1,16 @@
 //needs fix
 export function onscrollSkew (){
-	var value 
+	var value = -5;
 	var skewed = document.querySelector('#nabidka-vyber')
-	var unskewed = document.querySelector('.unskewed')
-	if (value != 0) {
-		window.addEventListener('scroll', function () {
-			value = -5 + window.scrollY/60;
-			var unskewedValue = -5 - window.scrollY/60;
+	window.addEventListener('scroll', function () {
+		if (value < -0.73)
+		{
+			value = -10 + window.scrollY/60;
 			skewed.style.transform = "skewY(" + value + "deg)"
-			unskewed.style.transform = "skewY(-" + value + "deg)"
 			console.log(value)
-		})
-	} else {
-		skewed.style.transform = "skewY(0deg)"
-	}
+		}
+
+		}
+		)
 	
 }

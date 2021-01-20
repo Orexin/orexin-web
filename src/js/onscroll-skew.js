0e060d6e4a33cc7b761eda5dfdfcb8f8ae408630
turniXXD <legokubik@seznam.cz>
2021-01-20 22:06:29 +0100

    nav-update

--- src/js/onscroll-skew.js
+++ src/js/onscroll-skew.js
@@ -1,5 +1,4 @@
-//needs fix
-<<<<<<< HEAD
+/* //needs fix
 export function onscrollSkew (){
 	var value = -5;
 	var skewed = document.querySelector('#nabidka-vyber')
@@ -15,21 +14,4 @@ export function onscrollSkew (){
 		)
 	
 }
-=======
-export function onscrollSkew() {
-	var value;
-	var skewed = document.querySelector('#nabidka-vyber');
-	var unskewed = document.querySelector('.unskewed');
-	if (value != 0) {
-		window.addEventListener('scroll', function () {
-			value = -5 + window.scrollY / 60;
-			var unskewedValue = -5 - window.scrollY / 60;
-			skewed.style.transform = 'skewY(' + value + 'deg)';
-			unskewed.style.transform = 'skewY(-' + value + 'deg)';
-			console.log(value);
-		});
-	} else {
-		skewed.style.transform = 'skewY(0deg)';
-	}
-}
->>>>>>> 07eb099bdddfb77a33e86458fb02147972f29116
+ */

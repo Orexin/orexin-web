ee5ad142a78c173a9ac6f3e9f583d8ebbed97fe9
turniXXD <legokubik@seznam.cz>
2021-01-20 01:20:34 +0100

    bruh-v-pici-tohle

--- src/js/onscroll-skew.js
+++ src/js/onscroll-skew.js
@@ -1,8 +1,18 @@
+//needs fix
 export function onscrollSkew (){
-	var skewed = document.getElementById('nabidka-vyber')
-	window.addEventListener('scroll', function () {
-		var value = -5 + window.scrollY/60;
-		skewed.style.transform = "skewY(" + value + "deg)"
-	})
-	console.log('kokot')
+	var value 
+	var skewed = document.querySelector('#nabidka-vyber')
+	var unskewed = document.querySelector('.unskewed')
+	if (value != 0) {
+		window.addEventListener('scroll', function () {
+			value = -5 + window.scrollY/60;
+			var unskewedValue = -5 - window.scrollY/60;
+			skewed.style.transform = "skewY(" + value + "deg)"
+			unskewed.style.transform = "skewY(-" + value + "deg)"
+			console.log(value)
+		})
+	} else {
+		skewed.style.transform = "skewY(0deg)"
+	}
+	
 }

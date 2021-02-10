438acc8c4912b8d8f3c36e653afb31d5f36d8451
Tassilo Balbo <contact@filipsikora.com>
2021-02-10 01:15:04 +0100

    faq animace

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,12 +4,12 @@ Array.from(arrows).forEach((item) => {
 	item.addEventListener('click', (event) => {
 		var arrowIcon = item.getElementsByClassName('faq-icon');
 		var content = item.closest('.faq-question-title').nextElementSibling;
-		if (!content.classList.contains('show')) {
+		if (!content.classList.contains('faq-show')) {
 			arrowIcon[0].style.transform = 'rotate(180deg)';
-			content.classList.toggle('show');
+			content.classList.toggle('faq-show');
 		} else {
 			arrowIcon[0].style = '';
-			content.classList.toggle('show');
+			content.classList.toggle('faq-show');
 		}
 	});
 });

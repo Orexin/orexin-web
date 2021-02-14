546dab19bf5d3e3eabae2d384bf92c4576de63b3
valajczech <jiri.vala11@gmail.com>
2021-02-14 03:09:40 +0100

    🚧 CI/CD testing spam 7

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-var sayHello = require('../src/js/main').sayHello;
+//var sayHello = require('../src/js/main').sayHello;
 var assert = require('chai').assert;
+var test = require('../src/components/footer').test;
 
 describe('App', () => {
 	it('App should return true', () => {
-		let result = sayHello();
-		assert.equal(result, 'true');
+		let result = test();
+		assert.equal(result, 'Footer works');
 	});
 });

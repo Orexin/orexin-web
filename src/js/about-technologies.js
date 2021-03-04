08cc9b603630f1872a447425ff66d414adbf8bbc
turniXXD <legokubik@seznam.cz>
2021-03-04 18:32:24 +0100

    about-technologie-commit-pred-explozi-notasu

--- src/js/about-technologies.js
+++ src/js/about-technologies.js
@@ -5,11 +5,17 @@ const imgContainer = document.getElementsByClassName('technologies-img-container
 const imgReversedContainer = document.getElementsByClassName('technologies-img-reversed-container');
 const outerElm = document.getElementsByClassName('technologie-presentation-container');
 const tech = document.getElementById('technologie-desc');
-//default 0.15
-var speed = 0.5;
+//default speed 0.15
+var imgXLowest = img[0].x,
+	speed = 0.5;
 var logos = new Array(),
-	logosReversed = new Array();
+	logosReversed = new Array(),
+	imgX = [new Array()],
+	imgXNegative = new Array();
 console.log(outerElm[0].getBoundingClientRect().top);
+console.log(img[10].x);
+console.log(img[5].x);
+console.log(img.x);
 
 //asign values to js object
 for (let i = 0; i < img.length; i++) {
@@ -17,9 +23,10 @@ for (let i = 0; i < img.length; i++) {
 		id: i,
 		x: 0,
 		y: 0,
-		width: img[i].offsetWidth,
-		height: img[i].offsetHeight,
+		originalX: img[i].x,
+		originalY: img[i].y,
 	};
+	imgX[i] = img[i].x;
 }
 //asign values to js object for reversed movement imgs
 for (let i = 0; i < imgReversed.length; i++) {
@@ -34,6 +41,22 @@ for (let i = 0; i < imgReversed.length; i++) {
 }
 console.log(logos, logosReversed);
 
+//Lowest img
+//console.log(Math.abs.apply(Math, imgX));
+console.log(imgX);
+for (let i = 0; i < imgX.length; i++) {
+	if (imgX[i] < 0) {
+		imgXNegative[i] = imgX[i];
+	}
+}
+console.log(imgXNegative);
+/* for (let i = 0; (i = imgXNegative.length); i++) {
+	Math.abs.apply(Math, imgXNegative);
+}
+console.log(imgXNegative); */
+//imgXLowest = Math.min(imgX);
+//console.log(imgXLowest);
+
 //get x and y where id = let i and then add 10 to it => diagonal movement
 document.addEventListener('DOMContentLoaded', function (event) {
 	//sleep function
@@ -42,6 +65,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
 	};
 	const presentationMovement = async () => {
 		var iter = 1;
+		let indent = 15;
 		while (true) {
 			for (let i = 0; i < img.length; i++) {
 				//move elems diagonaly up
@@ -50,16 +74,20 @@ document.addEventListener('DOMContentLoaded', function (event) {
 				logos[i].y += speed;
 				// collision detection and move down
 				for (let o = 0; o < outerElm.length; o++) {
-					if (outerElm[o].getBoundingClientRect().top + imgContainer[i].offsetTop + img[i].height <= outerElm[o].getBoundingClientRect().top) {
-						img[i].style.transform = `translateX(${outerElm[o].getBoundingClientRect().bottom - 300}px) translateY(${
-							outerElm[o].getBoundingClientRect().bottom + 300
-						}px) rotateZ(135deg)`;
-						logos[i].x = 0;
-						logos[i].y = 0;
+					// inkrement indent
+					if (img[0].getBoundingClientRect().right - img[0].getBoundingClientRect().width >= outerElm[o].getBoundingClientRect().right) {
+						indent += 15;
+						console.log('last collision');
 					}
-					//console.log(img[i].offsetTop);
+					if (img[i].getBoundingClientRect().right - img[i].getBoundingClientRect().width >= outerElm[o].getBoundingClientRect().right) {
+						img[i].style.transform = `translateX(-${outerElm[o].getBoundingClientRect().width - img[i].getBoundingClientRect().width - indent}px) translateY(-${
+							outerElm[o].getBoundingClientRect().width - img[i].getBoundingClientRect().width - indet
+						}px) rotateZ(135deg)`;
+
+						logos[i].x -= outerElm[o].getBoundingClientRect().width - img[i].getBoundingClientRect().width - indent;
+						logos[i].y -= outerElm[o].getBoundingClientRect().width - img[i].getBoundingClientRect().width - indent;
+					}
 				}
-				//console.log(img[i].y);
 			}
 			for (let i = 0; i < imgReversed.length; i++) {
 				//check if elements touched the top of client element

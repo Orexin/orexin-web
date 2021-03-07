00dc0ff187fc414fa8eeb3c651804281d59e492e
Tassilo Balbo <contact@filipsikora.com>
2021-03-08 00:34:58 +0100

    🚀 contact form almost done finally yay

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,1 +1,16 @@
 import './../css/contact.css';
+
+const checkLabels = document.querySelectorAll('.selectBox > label');
+
+Array.from(checkLabels).forEach((item) => {
+	item.addEventListener('click', (event) => {
+		const checkbox = item.previousElementSibling;
+		console.log(checkbox);
+		if (checkbox.checked) {
+			checkbox.checked = true;
+		} else {
+			checkbox.checked = false;
+		}
+		console.log(checkbox.checked);
+	});
+});

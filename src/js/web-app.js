0f203e3959ae4a2ba441eda8900b41d2cc120e8e
Tassilo Balbo <contact@filipsikora.com>
2021-04-26 02:47:14 +0200

    🤌 mini-contact component + FAQ contact form

--- src/js/web-app.js
+++ src/js/web-app.js
@@ -1,4 +1,5 @@
-import './../css/contact.css';
+import './../components/mini-contact';
+
 // SVGs
 import svgApp from './../img/svg-services/svg-web-app.svg';
 import svgEasy from './../img/svg-services/svg-inteligentni-a-jednoducha.svg';
@@ -66,37 +67,39 @@ var reaktivita = new Waypoint({
 });
 
 // onscroll elements 20 degrees translate
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
 	// if there is already transform attr in <g> tag create another group inside it, put everything there and then move the transform attr to the lower group
 	// defaults
-	var scroll=window.scrollY
-	var length=scroll
-	var translateRatioX=0, translateRatioY=0, degrees=20
+	var scroll = window.scrollY;
+	var length = scroll;
+	var translateRatioX = 0,
+		translateRatioY = 0,
+		degrees = 20;
 
-	if(scroll <= 500) {
+	if (scroll <= 500) {
 		// graph top
-		translateRatioX = Math.sin(degrees) * length*0.05;
-		translateRatioY = Math.sqrt(((length*0.05)*(length*0.05))+(translateRatioX*translateRatioX))
-		document.getElementById('svg-sluzby-web-app-graph-top').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`
+		translateRatioX = Math.sin(degrees) * length * 0.05;
+		translateRatioY = Math.sqrt(length * 0.05 * (length * 0.05) + translateRatioX * translateRatioX);
+		document.getElementById('svg-sluzby-web-app-graph-top').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`;
 
 		// left column
-		translateRatioX = Math.sin(degrees) * length*0.075;
-		translateRatioY = Math.sqrt(((length*0.075)*(length*0.075))+(translateRatioX*translateRatioX))
-		document.getElementById('svg-sluzby-web-app-left-column').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`
+		translateRatioX = Math.sin(degrees) * length * 0.075;
+		translateRatioY = Math.sqrt(length * 0.075 * (length * 0.075) + translateRatioX * translateRatioX);
+		document.getElementById('svg-sluzby-web-app-left-column').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`;
 
 		// values
-		translateRatioX = Math.sin(degrees) * length*0.1;
-		translateRatioY = Math.sqrt(((length*0.1)*(length*0.1))+(translateRatioX*translateRatioX))
-		document.getElementById('svg-sluzby-web-app-values').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`
+		translateRatioX = Math.sin(degrees) * length * 0.1;
+		translateRatioY = Math.sqrt(length * 0.1 * (length * 0.1) + translateRatioX * translateRatioX);
+		document.getElementById('svg-sluzby-web-app-values').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`;
 
 		// graph right top
-		translateRatioX = Math.sin(degrees) * length*0.125;
-		translateRatioY = Math.sqrt(((length*0.125)*(length*0.125))+(translateRatioX*translateRatioX))
-		document.getElementById('svg-sluzby-web-app-graph-right-top').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`
+		translateRatioX = Math.sin(degrees) * length * 0.125;
+		translateRatioY = Math.sqrt(length * 0.125 * (length * 0.125) + translateRatioX * translateRatioX);
+		document.getElementById('svg-sluzby-web-app-graph-right-top').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`;
 
 		// graph right bottom
-		translateRatioX = Math.sin(degrees) * length*0.15;
-		translateRatioY = Math.sqrt(((length*0.15)*(length*0.15))+(translateRatioX*translateRatioX))
-		document.getElementById('svg-sluzby-web-app-graph-right-bottom').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`
+		translateRatioX = Math.sin(degrees) * length * 0.15;
+		translateRatioY = Math.sqrt(length * 0.15 * (length * 0.15) + translateRatioX * translateRatioX);
+		document.getElementById('svg-sluzby-web-app-graph-right-bottom').style.transform = `translate(${translateRatioY}px, ${translateRatioX}px)`;
 	}
 });

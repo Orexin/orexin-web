233e9ad8c83a094100be38cd5bcbe3a67797d269
TurniXXD <legokubik@seznam.cz>
2021-05-07 19:03:01 +0200

    partnership-team

--- src/js/partnership.js
+++ src/js/partnership.js
@@ -7,8 +7,11 @@ import svgAdvertisement from './../img/partnership-advertisement.svg';
 import svgComission from './../img/partnership-comission.svg';
 import svgDiscount from './../img/partnership-discount.svg';
 import svgSupport from './../img/partnership-support.svg';
+import svgTeam from './../img/partnership-team.svg';
+
 
 // inject SVGs
+document.getElementById('svg-team').innerHTML = svgTeam;
 document.getElementById('svg-advertisement').innerHTML = svgAdvertisement;
 document.getElementById('svg-comission').innerHTML = svgComission;
 document.getElementById('svg-discount').innerHTML = svgDiscount;
@@ -27,6 +30,17 @@ var partnershipProc = new Waypoint({
 		document.getElementById('partnerstvi-proc-card-3').style.opacity = 1;
 		document.getElementById('partnerstvi-proc-card-4').style.transform = 'translateY(0)';
 		document.getElementById('partnerstvi-proc-card-4').style.opacity = 1;
+		// animation happens only once
+		this.destroy();
+	},
+});
+
+var partnershipTeam = new Waypoint({
+	element: document.getElementById('partnerstvi-team'),
+	offset: '90%',
+	handler: function () {
+		document.getElementById('svg-team').style.transform = 'translateY(0)';
+		document.getElementById('svg-team').style.opacity = 1;
 		// animation happens only once
 		this.destroy();
 	},

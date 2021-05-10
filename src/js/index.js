a1d69becf294c1a7bacbd94190c0cb20f0981758
TurniXXD <legokubik@seznam.cz>
2021-05-10 18:15:43 +0200

    🎅 index-typy-reseni_index-cards-waypoints

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
 import '../css/index.css';
 import './../components/mini-contact';
+import 'waypoints/lib/noframework.waypoints.min.js';
 import '@splidejs/splide/dist/css/splide.min.css';
 import Splide from '@splidejs/splide';
+
+// SVGs
+import svgWebPres from './../img/index-web-pres.svg';
+import svgWebApp from './../img/index-web-app.svg';
+import svgEcommerce from './../img/index-ecommerce.svg';
+
+// inject SVGs
+document.getElementById('svg-web-pres').innerHTML = svgWebPres;
+document.getElementById('svg-web-app').innerHTML = svgWebApp;
+document.getElementById('svg-ecommerce').innerHTML = svgEcommerce;
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
@@ -12,6 +23,22 @@ if ('serviceWorker' in navigator) {
 	  });
 	});
   }
+
+// Waypoints
+var nabidka = new Waypoint({
+	element: document.getElementById('nabidka'),
+	offset: '90%',
+	handler: function () {
+		document.getElementById('card-1').style.transform = 'translateY(0)';
+		document.getElementById('card-1').style.opacity = 1;
+		document.getElementById('card-2').style.transform = 'translateY(0)';
+		document.getElementById('card-2').style.opacity = 1;
+		document.getElementById('card-3').style.transform = 'translateY(0)';
+		document.getElementById('card-3').style.opacity = 1;
+		// animation happens only once
+		this.destroy();
+	},
+});
 
 document.addEventListener('DOMContentLoaded', function () {
 	var elms = document.getElementsByClassName('splide');

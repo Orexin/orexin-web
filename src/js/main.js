187d4bdb04efff6295f1362016e14a75906d4edf
Tassilo Balbo <contact@filipsikora.com>
2021-05-21 11:21:43 +0200

    😤 TOS + Privacy, WTF was that CSS about @TurniXXD

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,7 @@ import '../components/footer';
 import AOS from 'aos';
 // import * as underline from './shifting-underline';
 
-const preloader = document.getElementById('preloader')
+const preloader = document.getElementById('preloader');
 
 window.onload = function () {
 	// AOS
@@ -23,7 +23,7 @@ window.onload = function () {
 	if (document.title === 'Web-development na míru | Orexin Solutions') document.title = 'Orexin Solutions';
 	// yeet out preloader
 	preloader.style.display = 'none';
-	preloader.classList.remove('loading')
+	preloader.classList.remove('loading');
 
 	// sub-pages-paralax
 	window.addEventListener('scroll', function () {
@@ -37,19 +37,21 @@ window.onload = function () {
 	});
 
 	// scroll to top btn
-	window.onscroll = function() {scrollFunction()};
+	window.onscroll = function () {
+		scrollFunction();
+	};
 	var scrollupBtn = document.getElementById('scrollup-btn');
 
 	function scrollFunction() {
 		if (window.scrollY > 20) {
-			scrollupBtn.style.display = "block";
+			scrollupBtn.style.display = 'block';
 		} else {
-			scrollupBtn.style.display = "none";
+			scrollupBtn.style.display = 'none';
 		}
 	}
 
 	scrollupBtn.addEventListener('click', () => {
 		document.body.scrollTop = 0; // For Safari
 		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-	})
+	});
 };

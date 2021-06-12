a56440eec31a3df33d32829d3409b146af459cf9
TurniXXD <legokubik@seznam.cz>
2021-06-12 10:16:36 +0200

    🔒 respo_grid-two-columns-reverse

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,9 +11,15 @@ class Footer extends HTMLElement {
 		this.innerHTML = `
 	<div class="footer-content">
 		<div class="top-banner">
-			<h2>Připraveni?</h2>
-			<p>Sejdeme se, promluvíme si o tom a dohodneme se. </br> <strong>Kafe je na nás.</strong></p>
+			<div>
+				<h2>Připraveni?</h2>
+			</div>
+			<div>
+			 <p>Sejdeme se, promluvíme si o tom a dohodneme se. </br> <strong>Kafe je na nás.</strong></p>
+			</div>
+			<div>
 			<a href="/contact.html" class="footer-a">Jdeme na to</a>
+			</div>
 		</div>
 		<div class="upper">
 			<img src=${logoWhite} alt="Orexin White Logo" </div>
